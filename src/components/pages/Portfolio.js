916e719e811597894f20eac707c3b3c9a082a1c3
AKJ <kjajila@gmail.com>
2022-12-05 19:29:33 -0500

    add more inline styles

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -26,25 +26,24 @@ export default function Portfolio({ currentPage, handlePageChange }) {
                <div style={styles.cardContainer}>
                     <div style={styles.cards}>
                          <h2><span data-descr="It is a website to get posts about different sports">Sport spot</span></h2>
-                         <p>Technologies used:<span style={styles.spanColor}>Handlebars</span><span class="orange">Express</span><span class="green">Node</span><span class="blue">Sequelize</span> <span class="red">npm</span><span class="orange">ESLint</span><span class="blue">MySQL</span><span class="green">JavaScript</span><span class="blue">CSS</span></p>
+                         <p>Technologies used: Handlebars, Express, Node, Sequelize, npm, ESLint, MySQL, JavaScript, CSS</p>
                          <a href="https://the-sport-spot-team-1.herokuapp.com/" target="_blank" rel="noreferrer"><img src="./assets/images/SportSpot.png"alt="an image of 4 sports cards and a navigation bar"/></a>
                     </div>
                     <div style={styles.cards} class="first-item">
                          <h2><span data-descr="It is a website to get content about various topics and save our own notes">My Daily Almanac</span></h2>
-                         <p>Technologies used:<span class="orange">API</span><span class="green">JavaScript</span><span class="blue">CSS, BULMA</span> <span class="red">HTML</span></p>
+                         <p>Technologies used: API, JavaScript, CSS, BULMA, HTML</p>
                          <a href="https://mikeyboxx.github.io/my-daily-almanac/" target="_blank" rel="noreferrer"><img src="./assets/images/myDailyAlmanac.png"alt="an image of contents based on user selection and a drop up menu with dates"/></a>
                     </div>
                     <a href="#PortfolioFrontend"
                           className={currentPage === 'PortfolioFrontend'} onClick={() => handlePageChange('PortfolioFrontend')}>
                          <div style={styles.cards}>
-                              <h2><span data-descr="Frontend Apps">Frontend Applications</span></h2>
-                              <p>Technologies used:<span class="orange">API</span><span class="green">JavaScript jQuery</span><span class="blue">CSS</span> <span class="red">HTML</span></p>
-                  
+                              <h2><span data-descr="Frontend Apps">Front End Applications</span></h2>
+                              <p>Technologies used: API, JavaScript, jQuery, CSS, HTML</p>
                          </div>      
                     </a>
                     <div style={styles.cards}>
                          <h2><span data-descr="Back End Projects">Back End Projects</span></h2>
-                         <p><span class="blue">MySQL</span> <span class="red">ORM (Sequelize)</span><span class="green">Insomnia Core</span><span class="orange">Express</span> <span class="blue">JSON</span> <span class="red">Node</span><span class="green">JavaScript</span></p>
+                         <p>Technologies used: MySQL, ORM (Sequelize), Insomnia Core, Express, JSON, Node, JavaScript</p>
                          {/* <a href="https://drive.google.com/file/d/1oy56ihCyHF0vsD6TgXlGO8SZUGfNxBld/edit" target="_blank" rel="noreferrer"><img src="./assets/images/readme.png"alt="an image of a sample readme fenerated using this app"/></a> */}
                     </div>
                </div>
