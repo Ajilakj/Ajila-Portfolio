8e23f427a897207b68d47d9458cd81f0b2893d12
AKJ <kjajila@gmail.com>
2022-12-15 16:59:08 -0500

    add Virtual Escape Room project in portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import almanac from '../../images/myDailyAlmanac.png'
-import sport from '../../images/SportSpot.png'
+import weatherDashboard from '../../images/weatherDashboard.png'
+import NoteTaker from '../../images/NoteTaker.png'
+import TeamProfileGenerator from '../../images/TeamProfileGenerator.png'
+import jate from '../../images/jate.png'
+import virtualescaperoom from '../../images/virtualescaperoom.png'
+// import sport from '../../images/SportSpot.png'
 
 const styles = {
 container: {
@@ -38,33 +43,47 @@ export default function Portfolio({ currentPage, handlePageChange }) {
        <section style={styles.container}>
                <div >
                     <fieldset style={styles.cardContainer}>
+                         <legend>FULLSTACK APPLICATIONS</legend>
+                         <div style={styles.cards}>
+                                   <h2><span data-descr="It is a game app">Virtual Escape Room </span></h2>
+                                   <p>Technologies used:</p><p><span class="orange">React </span><span class="orange">MongoDB </span><span class="orange">Express </span> <span class="blue">JSON </span> <span class="red">Node </span><span class="green">JavaScript </span></p>
+                                   <a href=" https://virtual-escape-room-mikeyboxx.herokuapp.com/" target="_blank" rel="noreferrer"><img src={virtualescaperoom} style={styles.img} alt="an image of the virtual escape room game"/></a>
+                              </div>
+                              <div style={styles.cards}>
+                                   <h2><span data-descr="It is a text editor">JATE </span></h2>
+                                   <p>Technologies used:</p><p><span class="orange">MongoDB </span><span class="orange">Express </span> <span class="blue">JSON </span> <span class="red">Node </span><span class="green">JavaScript </span></p>
+                                   <a href="https://frozen-scrubland-82058.herokuapp.com/" target="_blank" rel="noreferrer"><img src={jate} style={styles.img} alt="an image of the text editor"/></a>
+                              </div>
+
+                    </fieldset>
+                    <fieldset style={styles.cardContainer}>
                          <legend>FRONTEND APPLICATIONS</legend>
                          <div style={styles.cards}>
-                              <h2><span data-descr="It is a website to get content about various topics and save our own notes">My Daily Almanac</span></h2>
+                              <h2><span data-descr="It is a website to get content about various topics and save our own notes">My Daily Almanac </span></h2>
                               <p>Technologies used: API, JavaScript, CSS, BULMA, HTML</p>
                               <a href="https://mikeyboxx.github.io/my-daily-almanac/" target="_blank" rel="noreferrer"><img src={almanac} style={styles.img} alt="an image of contents based on user selection and a drop up menu with dates"/></a>
                          </div>
                          <div style={styles.cards}>
-                              <h2><span data-descr="Weather Dashboard">Weather Dashboard</span></h2>
-                              <p>Technologies used:<span class="orange">API</span><span class="green">JavaScript jQuery</span><span class="blue">CSS</span> <span class="red">HTML</span></p>
-                              <a href="https://ajilakj.github.io/WeatherDashboard/" target="_blank" rel="noreferrer"><img src="./assets/images/weatherDashboard.png"alt="weather dashboard image with city name temp, wind and humidity"/></a>
+                              <h2><span data-descr="Weather Dashboard">Weather Dashboard </span></h2>
+                              <p>Technologies used:<span class="orange">API </span><span class="green">JavaScript jQuery </span><span class="blue">CSS </span> <span class="red">HTML </span></p>
+                              <a href="https://ajilakj.github.io/WeatherDashboard/" target="_blank" rel="noreferrer"><img src={weatherDashboard} style={styles.img} alt="weather dashboard image with city name temp, wind and humidity"/></a>
                          </div>
                     </fieldset>
                     <fieldset style={styles.cardContainer}>
                          <legend>BACKEND APPLICATIONS</legend>
                          <div style={styles.cards}>
-                         <h2><span data-descr="User can Save, Edit Delete notes">Note Taker</span></h2>
-                         <p>Technologies used:</p><p><span class="orange">Express</span> <span class="blue">JSON</span> <span class="red">Node</span><span class="green">JavaScript</span></p>
-                         <a href="https://murmuring-sands-90674.herokuapp.com/" target="_blank" rel="noreferrer"><img src="./assets/images/NoteTaker.png"alt="An image of note taker app with added items on the left side and selected item on the right"/></a>
+                         <h2><span data-descr="User can Save, Edit Delete notes">Note Taker </span></h2>
+                         <p>Technologies used:</p><p><span class="orange">Express </span> <span class="blue">JSON </span> <span class="red">Node </span><span class="green">JavaScript </span></p>
+                         <a href="https://murmuring-sands-90674.herokuapp.com/" target="_blank" rel="noreferrer"><img src={NoteTaker} style={styles.img} alt="An image of note taker app with added items on the left side and selected item on the right"/></a>
                     </div>
                     <div style={styles.cards}>
-                         <h2><span data-descr="Manager can enter team member's details, Based on the input an html file will generate">Team Profile Generator</span></h2>
-                         <p>Technologies used:</p><p><span class="orange">Jest</span> <span class="blue">inquirer</span> <span class="red">Node</span><span class="green">JavaScript</span></p>
-                         <a href="https://drive.google.com/file/d/1fkdbNH3y3JD7HKvWkfk7MPmFh8GI31go/view" target="_blank" rel="noreferrer"><img src="./assets/images/TeamProfileGenerator.png"alt="an image of a sample readme fenerated using this app"/></a>
+                         <h2><span data-descr="Manager can enter team member's details, Based on the input an html file will generate">Team Profile Generator </span></h2>
+                         <p>Technologies used:</p><p><span class="orange">Jest </span> <span class="blue">inquirer </span> <span class="red">Node </span><span class="green">JavaScript </span></p>
+                         <a href="https://drive.google.com/file/d/1fkdbNH3y3JD7HKvWkfk7MPmFh8GI31go/view" target="_blank" rel="noreferrer"><img src={TeamProfileGenerator} style={styles.img} alt="an image of a sample readme fenerated using this app"/></a>
                     </div>
                     </fieldset>
                     {/* <div style={styles.cards}>
-                         <h2><span data-descr="It is a website to get posts about different sports">Sport spot</span></h2>
+                         <h2><span data-descr="It is a website to get posts about different sports">Sport spot </span></h2>
                          <p>Technologies used: Handlebars, Express, Node, Sequelize, npm, ESLint, MySQL, JavaScript, CSS</p>
                          <a href="https://the-sport-spot-team-1.herokuapp.com/" target="_blank" rel="noreferrer"><img src={sport} style={styles.img} alt="an image of 4 sports cards and a navigation bar"/></a>
                     </div> */}
