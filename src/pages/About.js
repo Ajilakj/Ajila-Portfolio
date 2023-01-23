14d38a3fdf7e37ab66bfdb294020d7ae31a2b4a0
AKJ <kjajila@gmail.com>
2023-01-22 20:05:49 -0500

    add inner-wrapper class to set max width

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import profile from '../images/Ajila.JPG'
-import './style.css';
 export default function About() {
   return (
-    <main id="about">
-      <h2>About Me</h2>
-      <section id="aboutMe">
-        <img src={profile} alt="My profile picture"/>
-        <p>
-            <b>Hi, My name is Ajila.</b> <br/>
-              I am a Fullstack coding bootcamp gradute from Columbia Engineering.<br/>
-             As a fullstack developer I am familiar with the technologies like React, Express, MongoDB, Node, JS, .................. This portfoilio will express some of my works which I am really proud of.
-        </p>
-      </section>
-    </main>
+    <section className="inner-wrapper">
+        <h2>About Me</h2>
+        <section id="aboutMe">
+          <img src={profile} alt="My profile picture"/>
+          <p>
+              <b>Hi, My name is Ajila.</b> <br/>
+                I am a Fullstack coding bootcamp gradute from Columbia Engineering.<br/>
+              As a fullstack developer I am familiar with the technologies like React, Express, MongoDB, Node, JS, .................. This portfoilio will express some of my works which I am really proud of.
+          </p>
+        </section>
+    </section>
   );
 }
