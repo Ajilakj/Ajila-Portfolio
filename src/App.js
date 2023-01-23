653ebba6d9ea1bc27082272890ad9dba3ebeb317
AKJ <kjajila@gmail.com>
2023-01-22 20:40:38 -0500

    modify app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from "react";
 import Footer from "./components/Footer";
 import PortfolioContainer from "./components/PortfolioContainer";
-
-const styles = {
-     container: {
-       margin:'0px',
-       background: '#EDF1FF',
-       fontFamily:'Arial, Helvetica, sans-serif',
-       maxWidth:'1200px'
-     }
-   }
    
 const App = () =>{
 return(
