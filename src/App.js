6d7a7358443bccc740bf7d8c01804866ee4a351a
AKJ <kjajila@gmail.com>
2023-02-08 15:45:32 -0500

    change styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import Footer from "./components/Footer";
-import PortfolioContainer from "./components/PortfolioContainer";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout"
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
    
 const App = () =>{
 return(
-          <>
-               <PortfolioContainer />
-               <Footer/>
-          </> 
+
+<Routes>
+      <Route path="/Ajila-Portfolio" element={<Layout />} >
+          <Route index element={<About />} />
+          <Route path="about-me" element={<About />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="contact-me" element={<Contact />} />
+          <Route path="resume" element={<Resume />} />
+     </Route>
+
+    </Routes>
+
      )
 }
 export default App;
