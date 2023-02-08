e24c698091d4e5aa586580adc066fb646fae8d29
AKJ <kjajila@gmail.com>
2023-02-08 15:49:52 -0500

    modify components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Header({ currentPage, handlePageChange }) {
+function Header({currentPage}) {
   return (
 
     <header>
-      <div className='innerWraper' id="navBarContainer">
-        <h1>Ajila's Portfolio</h1>
+      <div className='inner-wrapper' id="navBarContainer">
+        <div class="logo-wrapper">Ajila's Portfolio</div>
         <ul id="navBar">
           <li className="nav-item">
-            <a
-              href="#about"
-              onClick={() => handlePageChange('About')}              
-              className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-            >
-              About Me
-            </a>
+         
+            <Link to="about-me">About Me</Link>
           </li>
           <li className="nav-item">
-            <a
-              href="#portfolio"
-              onClick={() => handlePageChange('Portfolio')}
-              className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-            >
-              Portfolio
-            </a>
+            <Link to="portfolio">Portfolio</Link>
+           
           </li>
           <li className="nav-item">
-            <a 
-              href="#contact"
-              onClick={() => handlePageChange('Contact')}
-              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-            >
-              Contact
-            </a>
+          <Link to="contact-me">Contact</Link>
+           
           </li>
           <li className="nav-item">
-            <a
-              href="#resume"
-              onClick={() => handlePageChange('Resume')}
-              className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-            >
-              Resume
-            </a>
+            <Link to="resume">Resume</Link>
           </li>
         </ul>
       </div>
