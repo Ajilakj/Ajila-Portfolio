a1fa295a2b40dc407570186db92902e3b375a5fe
AKJ <kjajila@gmail.com>
2023-02-10 12:49:28 -0500

    add linkedin git icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
      return (
@@ -8,17 +10,19 @@ function Footer() {
             <a 
               href="https://github.com/Ajilakj" target="_blank" rel="noreferrer"
             >
-              GitHub
+               <FontAwesomeIcon icon={faGithub} />  GitHub
+              
             </a>
           </li>
           <li>
               &copy; Ajila K Johnkutty @ 2022
           </li>
           <li>
+          
             <a 
               href="https://www.linkedin.com/in/ajila-k-johnkutty/" target="_blank" rel="noreferrer"
             >
-              LinkedIn
+              <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
             </a>
           </li>
         </ul>
